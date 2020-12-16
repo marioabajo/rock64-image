a1215d75d5c726ebcf6c4ae4f86b15880e9312cc
Mario Abajo <marioabajo@gmail.com>
2020-12-16 01:06:50 +0100

    Updated buildroot image to 2020.11 Added conmon package Added podman package

--- build-image/build.sh
+++ build-image/build.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+VERS=2020.11
+
 # Deploy buildroot framework
 if [ ! -e buildroot/Makefile ]; then
-  wget https://buildroot.org/downloads/buildroot-2020.05.tar.gz; \
-  tar -zxpvf buildroot-2020.05.tar.gz; \
-  cp -a buildroot-2020.05/. buildroot
-  rm -rf buildroot-2020.05
+  wget https://buildroot.org/downloads/buildroot-"$VERS".tar.gz; \
+  tar -zxpvf buildroot-"$VERS".tar.gz; \
+  cp -a buildroot-"$VERS"/. buildroot
+  rm -rf buildroot-"$VERS"
 fi
 
 
 # Download from repository the latest configuration and patches
-if [ "$UPDATE_CONFIG" == 1 ]; then
+if [ "$UPDATE_CONFIG" == 1 ] || [ ! -e buildroot/.config ]; then
 	git clone https://github.com/marioabajo/rock64-image
 	cp -av rock64-image/build-image/buildroot/. buildroot
 	cp -av rock64-image/build-image/buildroot/buildroot.config buildroot/.config
+	if [ -e buildroot/patches ] && [ ! -e buildroot/patches/.applied ]; then
+		cd buildroot
+		for p in patches/*; do
+			patch -p1 < "$p"
+		done && touch patches/.applied
+		cd ..
+	fi
 	rm -rf rock64-image
 fi
 
