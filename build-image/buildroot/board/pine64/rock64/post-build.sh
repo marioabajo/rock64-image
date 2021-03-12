18a289042f80e5acfcf84128e9c4d3565e62f85f
Mario Abajo <marioabajo@gmail.com>
2021-03-12 02:08:13 +0100

    Change syslinux bootscript (extlinux.conf) by an script. This is the firs step for auto-rollback feature and for kernel params customization preinit fix

--- build-image/buildroot/board/pine64/rock64/post-build.sh
+++ build-image/buildroot/board/pine64/rock64/post-build.sh
@@ -6,4 +6,7 @@ BOARD_DIR="$(dirname $0)"
 $MKIMAGE -n rk3328 -T rksd -d $BINARIES_DIR/u-boot-tpl.bin $BINARIES_DIR/u-boot-tpl.img
 cat $BINARIES_DIR/u-boot-tpl.img $BINARIES_DIR/u-boot-spl.bin > $BINARIES_DIR/u-boot-tpl-spl.img
 
-install -m 0644 -D $BOARD_DIR/extlinux.conf $TARGET_DIR/boot/extlinux/extlinux.conf
+# New boot system using boot script
+$MKIMAGE -A arm64 -O linux -T script -C none -a 0 -e 0 -n "boot script" -d boot.txt boot.scr
+install -m 0644 -D $BOARD_DIR/boot.scr $TARGET_DIR/boot/boot.scr
+# previous system # install -m 0644 -D $BOARD_DIR/extlinux.conf $TARGET_DIR/boot/extlinux/extlinux.conf
