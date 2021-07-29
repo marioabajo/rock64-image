2ba1556ecaca57d26258cc6ea74a1d624dc7cc89
turin <turin@wakizashi.local>
2021-07-29 02:31:37 +0200

    First attempt to split the toolchain generation from the board generation, this will accelerate tests Added ethaddr flag at boot to address the eth mac issue

--- build-image/build.sh
+++ build-image/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 # If buildroot version is not set, default to this one:
-[ -z $VERS ] && VERS=2021.02.1
+[ -z $VERS ] && VERS=2021.02.3
 
 # If no build command is supplied, default to build everything:
 [ -z $RUNCMD ] && RUNCMD="make"
@@ -14,12 +15,40 @@ if [ ! -e buildroot/Makefile ]; then
   rm -rf buildroot-"$VERS"
 fi
 
-
-# Download from repository the latest configuration and patches
-if [ "$UPDATE_CONFIG" == 1 ] || [ ! -e buildroot/.config ]; then
+if [ "$UPDATE_CONFIG" == 1 ] || [ ! -e "buildroot/.git_clone" ]; then
 	git clone https://github.com/marioabajo/rock64-image
 	git checkout master
 	git pull
+	touch buildroot/.git_clone
+fi
+
+# Generate the toolchain
+if [ ! -e "buildroot/.toolchain-rockore" ]; then
+	echo "###### Generating Toolchain..."
+	cp -av rock64-image/build-image/buildroot/buildroot-toolchain.config buildroot/.config
+	cd buildroot
+	make oldconfig
+	make source
+	make toolchain
+
+	echo "###### Toolchain generated, copying...."
+	TOOLCHAIN_NAME="toolchain-rockore-$(date +%Y%m%d%H%M).tar.gz"
+	cd output/host
+	tar -czpf "$TOOLCHAIN_NAME" .
+	cp "$TOOLCHAIN_NAME" ../
+	cd ../../
+	mkdir toolchain-rockore
+	cp -a output/host/. toolchain-rockore
+
+	echo "###### Cleaning buildroot environment..."
+	make clean
+	cd ..
+	touch buildroot/.toolchain-rockore
+fi
+
+# Download from repository the latest configuration and patches
+if [ ! -e "buildroot/.buildroot_board_env" ]; then
+	echo "###### Setting up buildroot environment..."
 	cp -av rock64-image/build-image/buildroot/. buildroot
 	cp -av rock64-image/build-image/buildroot/buildroot.config buildroot/.config
 	if [ -e buildroot/patches ] && [ ! -e buildroot/patches/.applied ]; then
@@ -32,9 +61,11 @@ if [ "$UPDATE_CONFIG" == 1 ] || [ ! -e buildroot/.config ]; then
 	rm -rf rock64-image
 fi
 
-
-# Start the build process
-cd buildroot
-make oldconfig
-make source
-$RUNCMD
+# Start the board build process
+if [ -e "buildroot/.buildroot_board_env" ]; then
+	echo "##### Board image building..."
+	cd buildroot
+	make oldconfig
+	make source
+	$RUNCMD
+fi
